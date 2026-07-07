00e87c35167561487c3546740fda9f857eeaf8ea
jhasegaw-student <145496630+jhasegaw-student@users.noreply.github.com>
2026-07-07 12:08:33 -0500

    revisions

---
+++ lectures/.ipynb_checkpoints/a12am_hw1-checkpoint.py
@@ -0,0 +1,11 @@
+def arithmetic(x, y):
+    """
+    Modify this code so that it performs one of four possible functions, 
+    as specified in the following table:
+
+                        isinstance(x,str)  isinstance(x,float)
+    isinstance(y,str)   return x+y         return str(x)+y
+    isinstance(y,float) return x*int(y)    return x*y
+    """
+    raise RuntimeError("You need to write this part!")
+
---
+++ lectures/a12am_hw2.py
@@ -0,0 +1,38 @@
+def words2characters(words):
+    """
+    This function converts a list of words into a list of characters.
+
+    @param:
+    words - a list of words
+
+    @return:
+    characters - a list of characters
+
+    Every element of "words" should be converted to a str, then split into
+    characters, each of which is separately appended to "characters." For 
+    example, if words==['hello', 1.234, True], then characters should be
+    ['h', 'e', 'l', 'l', 'o', '1', '.', '2', '3', '4', 'T', 'r', 'u', 'e']
+    """
+    raise RuntimeError("You need to write this part!")
+
+
+def next_birthday(date, birthdays):
+    '''
+    Find the next birthday after the given date.
+
+    @param:
+    date - a tuple of two integers specifying (month, day)
+    birthdays - a dict mapping from date tuples to lists of names, for example,
+      birthdays[(1,10)] = list of all people with birthdays on January 10.
+
+    @return:
+    birthday - the next day, after given date, on which somebody has a birthday
+    list_of_names - list of all people with birthdays on that date
+    '''
+    raise RuntimeError("You need to write this part!")
+    birthday = (1,1)
+    list_of_names = []
+    return birthday, list_of_names
+    
+
+
